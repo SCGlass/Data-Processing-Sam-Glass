004850a85195472c77bfe27d6b4252a52a2664d1
Sam Glass <sam.charles.glass@gmail.com>
2022-11-12 23:12:37 +0100

    bootstrap tutorial adding title and color to it

--- Practise_tests/Boot_strap_tutorial/complete_bootstrap.py
+++ Practise_tests/Boot_strap_tutorial/complete_bootstrap.py
@@ -1,12 +1,12 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Output, Input
-import plotly_express as px
-import dash_bootstrap_components as dbc
-import pandas_datareader.data as web
 import datetime
+
+import dash
+import dash_bootstrap_components as dbc
 import pandas as pd
+import pandas_datareader.data as web
+import plotly_express as px
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
 # start = datetime.datetime(2020, 1 , 1) # using datetime to select where to start
 # end = datetime.datetime(2020, 12, 3) # using datetime to select where to end
@@ -18,33 +18,44 @@ import pandas as pd
 # saving as csv file to work with so we dont overload from the API
 # df.to_csv("Practise_tests\Boot_strap_tutorial\mystocks.csv", index=False)
 
-df = pd.read_csv( "Practise_tests\Boot_strap_tutorial\mystocks.csv")  # Now importing dataframe from file
+df = pd.read_csv(
+    "Practise_tests\Boot_strap_tutorial\mystocks.csv"
+)  # Now importing dataframe from file
 print(df[:15])
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],  # using flask in background for the app adn setting the visual theme
-                meta_tags=[{'name': 'viewport',  # The next two lines responds to a mobile layout and makes it smaller for that
-                            'content': 'width=device-width, initial-scale=1.0'}],
-                )
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[
+        dbc.themes.BOOTSTRAP
+    ],  # using flask in background for the app adn setting the visual theme
+    meta_tags=[
+        {
+            "name": "viewport",  # The next two lines responds to a mobile layout and makes it smaller for that
+            "content": "width=device-width, initial-scale=1.0",
+        }
+    ],
+)
 
 
 #   Layout section: Bootstrap
 # ------------------------------------------------------------------------------------
-app.layout = dbc.Container([
-    dbc.Row([
-        dbc.Col(html.H1("Stock Market Dashboard"))
-    ]),
-
-    dbc.Row([
-
-
-    ]),
-
-    dbc.Row([
-
-
-    ])
-])
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H1(
+                        "Stock Market Dashboard",
+                        className="text-center text-primary, mb-4", # makes center and blue mb4 gives a little space under the title
+                    )
+                )  
+            ]
+        ),
+        dbc.Row([]),
+        dbc.Row([]),
+    ]
+)
 
 # need this code to run the app for dash
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run_server(debug=True, port=3000)
