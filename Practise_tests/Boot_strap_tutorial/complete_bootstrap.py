ced72a7964c6a80e50ca47706441f1c24b77305f
Sam Glass <sam.charles.glass@gmail.com>
2022-11-12 14:00:02 +0100

    added pandas datareader to pipfile

---
+++ Practise_tests/Boot_strap_tutorial/complete_bootstrap.py
@@ -0,0 +1,8 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
+import plotly_express as px
+import dash_bootstrap_components as dbc
+import pandas_datareader.data as web
+import datetime
